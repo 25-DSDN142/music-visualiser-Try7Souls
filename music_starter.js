1752448d112ba474c1206bfb548a8e6d6e2db665
Try7Souls <orlando66cooper@gmail.com>
2025-09-15 22:05:54 +1200

    Middle Circle
    circle that moves based on the vocals of the song

--- music_starter.js
+++ music_starter.js
@@ -2,74 +2,20 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
-  background(255)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER);
-  textSize(24);
-  strokeWeight(bass*0.5)
-  
+// Background
 
-let drumMap =parseInt( map(drum, 0,100,30,90));
-let lineLength = 300;
-let lineStart = 100;
-let lineEnd = lineStart+lineLength;
+let bgShade = (0, 0)
+background(bgShade)
 
- 
+rectMode(CENTER);
+ellipseMode(CENTER);
 
-colorMode(HSB);
-fill(0, 100, 100);  
+// Middle circle that changes size with the vocals
 
-stroke(drumMap,80,80)
-
-  
-
-  for(let i =1; i < 15; i++){
-   let lineStep = i*50;
-   line(lineStart, lineStep, lineEnd, lineStep)
-  
-
-  }
-  
-
-
+let aura = map(vocal, 0, 100, 150, 400);
+noFill();
+stroke(0, 200, 255, 150);
+strokeWeight(6);
+ellipse(width / 2, height / 2, aura);
 
 }
-
-
-
-
-
-   //let bar_spacing = height / 10;
-   //let bar_height = width / 10;
-   //let bar_pos_x = width / 2;
- 
-// changes 
-   // vocal bar is red
-   //fill(200, 0, 0);
-   //rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   //fill(0);
-   //text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   //fill(0, 200, 0);
-   //rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   //fill(0);
-   //text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   //fill(50, 50, 240);
-   //rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   //fill(0);
-   //text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   //fill(200, 200, 200);
-   //rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   //fill(0);
-   //text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   //fill(255, 255, 0);
- 
-   // display "words"
-   //textAlign(CENTER);
-   //textSize(vocal);
-   //text(words, width/2, height/3);
